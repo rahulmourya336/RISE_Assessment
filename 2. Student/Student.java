502d9f8c4e9e00c08943ab65ce665174b2dd5588
RahulMourya <rahulmourya336@gmail.com>
2018-06-24 12:17:36 +0530

    2.student - One Module Pending

--- 2. Student/Student.java
+++ 2. Student/Student.java
@@ -81,6 +81,31 @@ public class Student {
         return maxIndex;
     }
 
+    public static void startsWith(Student[] students, int limit) {
+        for (int loop = 0; loop < limit; loop++) {
+            if (students[loop].getName().startsWith("S")) {
+                System.out.println("Name: " + students[loop]
+                        .getName() + " CPI: " + students[loop].getCpi());
+            }
+        }
+    }
+
+    public static int[] returnMaxIndex(Student[] array) {
+        int[] maxIndexes = new int[array.length];
+        int index = 0;
+        for (int array1 = 0; array1 < array.length; array1++) {
+            for (int array2 = 0; array2 < array.length - 1; array2++) {
+                if (array[array2].getHeight() < array[array2 + 1].getHeight()) {
+                    maxIndexes[index] = array2;
+                } else {
+                    maxIndexes[index] = array2 + 1;
+                }
+                index++;
+            }
+        }
+        return maxIndexes;
+    }
+
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
         List<Integer> MaleIndex = new ArrayList<>();
@@ -130,18 +155,14 @@ public class Student {
         maxMaleIndex = returnMaxIndex(MaleIndex, students);
         maxFemaleIndex = returnMaxIndex(FemaleIndex, students);
 
-        System.out.println("Highest Male scorer: " + students[maxMaleIndex]);
+        System.out.println("\nHighest Male scorer: " + students[maxMaleIndex]);
         System.out.println("Highest Female scorer: " + students[maxFemaleIndex]);
 
         //Sort them based on heights - Pending
-        
-//        for(int loop=0; loop < limit; loop++){
-//            Arrays.sort(students[loop[height]].getHeight());
-//        }
 //        int tempIndex = 0;
-//        for (int first = 0; first < limit ; first++) {
+//        for (int first = 0; first < limit - 1; first++) {
 //            for (int next = 0; next < limit - 1; next++) {
-//                System.out.println("next: "+ next+ " Next+1:"+ (next + 1) + "tempIndex :"+ tempIndex);
+//                System.out.println("next: " + next + " Next+1:" + (next + 1) + "tempIndex :" + tempIndex);
 //                if (students[next].getHeight() >= students[(next + 1)].getHeight()) {
 //                    heightIndex[tempIndex] = next;
 //                } else {
@@ -150,19 +171,18 @@ public class Student {
 //                tempIndex++;
 //            }
 //        }
-        System.out.println("Height Index size: " + heightIndex.length);
-        for (int loop = 0; loop < heightIndex.length; loop++) {
-            System.out.println(students[heightIndex[loop]]);
-        }
+//        System.out.println("\n");
+//        int[] indexes = new int[limit];
+//        indexes = returnMaxIndex(students);
+//        for(int loop = 0; loop < indexes.length; loop++){
+//            System.out.println(students[indexes[loop]]);
+//        }
 
-        // Print Name and Height
+        System.out.println("\n-- Sorting based on height Pending -- ");
+
         System.out.println("\nStudent CPIs whoes name starts with \"S\": ");
         //CPI of those students whose name starts with "S"
-        for (int loop = 0; loop < limit; loop++) {
-            if (students[loop].getName().startsWith("S")) {
-                System.out.println("Name: " + students[loop]
-                        .getName() + " CPI: " + students[loop].getCpi());
-            }
-        }
+        startsWith(students, limit);
+
     }
 }
